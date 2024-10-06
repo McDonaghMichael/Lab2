32bcbe13ffe68c4786dbcbaa475c6b447e1e887f
Michael <93167717+McDonaghMichael@users.noreply.github.com>
2024-10-06 21:29:31 +0100

    Displaying current time in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
       <div>
-        <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </div>
   );
 }
