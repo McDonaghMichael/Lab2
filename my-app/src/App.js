de4b6cfddb36e885597c01999b1a38765b1d343e
Michael <93167717+McDonaghMichael@users.noreply.github.com>
2024-10-06 21:40:22 +0100

    Implemented routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,21 @@
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
     return (
-        <div>
-            <Header />
-            <Content />
-            <Footer />
-        </div>
+        <Router>
+            <div>
+                <NavigationBar />
+                <Routes>
+                    <Route path="/home" element={<Content />} />
+                    <Route path="/read" element={<h1>Read Component</h1>} />
+                    <Route path="/create" element={<h1>Create Component</h1>} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
--- my-app/src/components/NavigationBar.js
+++ my-app/src/components/NavigationBar.js
@@ -1,21 +1,22 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavigationBar = () => {
-  return (
-        <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="/">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/create">Create</Nav.Link>
-              <Nav.Link href="/read">Read</Nav.Link>
-            </Nav>
-          </Container>
-      </Navbar>
-  );
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/">Create</Nav.Link>
+                    <Nav.Link href="/">Read</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default NavigationBar;
